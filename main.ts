ad5b9468823f064e5f22a6c3a1b694d91b21fba1
randomguy048 <jkmaster236@gmail.com>
2026-01-14 09:45:58 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+let minutes_to_sleep = 0
+let hours_to_remind = 0
+basic.showLeds(`
+    # . # . #
+    # . # . .
+    # # # . #
+    # . # . #
+    # . # . #
+    `)
+basic.pause(1000)
+basic.clearScreen()
+basic.forever(function () {
+    hours_to_remind = 15.5
+    basic.pause(3600000)
+    while (0 < hours_to_remind) {
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            . # # # .
+            . . # . .
+            . # . . .
+            `)
+        music.play(music.stringPlayable("C D G F C D - - ", 600), music.PlaybackMode.UntilDone)
+        if (input.buttonIsPressed(Button.A)) {
+            music.play(music.stringPlayable("C - C5 - - - - - ", 500), music.PlaybackMode.UntilDone)
+            basic.clearScreen()
+            break;
+        }
+    }
+    hours_to_remind += -1
+    if (hours_to_remind == 0.5) {
+        hours_to_remind += -0.5
+        music.play(music.stringPlayable("C D G F C D - - ", 690), music.PlaybackMode.UntilDone)
+        basic.showNumber(4.5)
+    }
+    minutes_to_sleep = 510
+    if (minutes_to_sleep == 510) {
+        while (minutes_to_sleep > 0) {
+            basic.pause(1800000)
+            minutes_to_sleep += -30
+        }
+    } else {
+        minutes_to_sleep += 510
+    }
+})

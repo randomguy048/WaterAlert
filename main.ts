a2896ac11fc18eb480deee5e9d932f1df4df368f
randomguy048 <jkmaster236@gmail.com>
2026-01-27 11:58:26 -0800

    fixed bug where the reminder would not repeat after one dismissal, switched from one hour measurements to 30 minute cooldowns for reminders

--- main.ts
+++ main.ts
@@ -13,35 +13,24 @@ basic.clearScreen()
 basic.forever(function () {
     minutes_to_sleep = 510
     hours_to_remind = 15.5
-    basic.pause(5000)
-    while (0.5 < hours_to_remind) {
-        basic.showLeds(`
-            . # # # .
-            # . . . #
-            . # # # .
-            . . # . .
-            . # . . .
-            `)
-        music.play(music.stringPlayable("C D G F C D - - ", 600), music.PlaybackMode.UntilDone)
+    basic.pause(1800000)
+    while (0 < hours_to_remind) {
+        music.play(music.stringPlayable("C D G F C D G F ", 600), music.PlaybackMode.LoopingInBackground)
+        basic.showNumber(4.5)
         if (input.buttonIsPressed(Button.A)) {
-            hours_to_remind += -1
+            hours_to_remind += -0.5
+            music.stopMelody(MelodyStopOptions.All)
             basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
+                . . # # #
+                . . # . #
+                . . # # #
                 . . . . .
                 . . . . .
                 `)
             music.play(music.stringPlayable("C - C5 - - - - - ", 500), music.PlaybackMode.UntilDone)
             basic.clearScreen()
-            basic.pause(5000)
+            basic.pause(1800000)
             continue;
-        }
-        if (hours_to_remind == 0.5) {
-            hours_to_remind += -0.5
-            music.play(music.stringPlayable("C D G F C D - - ", 690), music.PlaybackMode.UntilDone)
-            basic.showNumber(4.5)
-            break;
         }
     }
     if (minutes_to_sleep == 510) {
@@ -57,6 +46,8 @@ basic.forever(function () {
             minutes_to_sleep += -30
         }
     } else {
+        music.play(music.stringPlayable("- - - - - - C D ", 70), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("B A G F E D C C5 ", 550), music.PlaybackMode.UntilDone)
         basic.clearScreen()
         minutes_to_sleep += 510
     }
